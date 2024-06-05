b6b3c3c93dee17b3a999b7e094d2f526dcd56c90
Toru Yoshimura <toru@office-yoshimura.com>
2024-06-05 10:09:43 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROMPT_COMMAND='PS1_CMD1=$(git branch --show-current 2>/dev/null)'; PS1='\u@\h: \w(${PS1_CMD1})\\$ '
+
+# ls
+export LS_OPTIONS='--color=auto'
+eval "$(dircolors)"
+alias ls='ls $LS_OPTIONS'
+alias ll='ls $LS_OPTIONS -l'
+alias l='ls $LS_OPTIONS -lA'
+
+# alias
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
