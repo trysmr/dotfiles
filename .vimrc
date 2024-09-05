190fcde63ae142f0708196b2ba15201e9af99076
Toru Yoshimura <219759@adk.jp>
2024-09-05 13:40:31 +0900

    Add .vimrc and .gitignore_global

---
+++ .vimrc
@@ -0,0 +1,6 @@
+set tabstop=2
+set autoindent
+set expandtab
+set shiftwidth=2
+set number
+syntax on
--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+#!/bin/bash
 
+dir="$(cd $(dirname $0) && pwd -P)"
+
+for f in $dir/.??*; do
+  filename=`basename $f`
+  [[ `basename $f` = ".git" ]] && continue
+
+  ln -snf $dir/$filename $HOME
+done
