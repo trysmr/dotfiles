32e0c2bac208abef08590b9292c5586e453e7c3d
Toru Yoshimura <toru@office-yoshimura.com>
2024-12-13 11:04:22 +0900

    .gitignore_globalを.config/git/ignoreに変更した

--- install.sh
+++ install.sh
@@ -6,5 +6,14 @@ for f in $dir/.??*; do
   filename=`basename $f`
   [[ `basename $f` = ".git" ]] && continue
 
+  # .configの場合はディレクトリを対象にする
+  if [ $filename = ".config" ]; then
+    for config in $dir/$filename/*; do
+      configname=`basename $config`
+      ln -snf $config $HOME/.config
+    done
+    continue
+  fi
+
   ln -snf $dir/$filename $HOME
 done
